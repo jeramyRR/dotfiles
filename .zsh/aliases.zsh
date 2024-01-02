0cf1b103d57c958de9aa627a0aa6bd4f5a40c347
Jeramy Singleton <jeramyRR@gmail.com>
2024-01-02 13:54:56 -0500

    Add more dotfiles

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,6 @@
+# Alias
+# ---
+#
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias grep='grep --color'
---
+++ .zshenv
@@ -0,0 +1,6 @@
+# Added locations to path variable
+export PATH=$PATH:$HOME/.local/bin:$HOME/.cargo/bin
+
+export EDITOR=vim
+
+. "$HOME/.cargo/env"
